e684b9aee48444add0fbda3ac1995749e5a8e64c
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-10-17 14:45:42 +0200

    I have exeded the limits of my mortal flesh update
    asdadafdshgddfhf

--- Sigurd/box-lighting/main.js
+++ Sigurd/box-lighting/main.js
@@ -18,22 +18,47 @@ const meterial = new THREE.MeshStandardMaterial({color: 0xdb0f24});
 const cube = new THREE.Mesh(geometry, meterial);
 scene.add(cube);
 
+const faceloader = new THREE.TextureLoader().load('portrait.jpg')
+const myfaceonabox = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({map: faceloader})
+)
+myfaceonabox.position.set(0,0,0)
+scene.add(myfaceonabox)
+
 //this puts light in the scene and position and stuff 
 const pointlight = new THREE.PointLight(0xffffff)
 pointlight.position.set(0,2,0)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointlight, )
+scene.add(pointlight, ambientLight)
 
 const lightHelper = new THREE.PointLightHelper(pointlight)
-scene.add(lightHelper)
+//scene.add(lightHelper)
 
 camera.position.z = 5;
 
+const sexymantex = new THREE.TextureLoader().load('https://images.nightcafe.studio/jobs/qTqpq1U3rKMx1Xj4qiXJ/qTqpq1U3rKMx1Xj4qiXJ--1--aezkk.jpg?tr=w-1600,c-at_max')
+scene.background = sexymantex;
+
 function animate() {
 	requestAnimationFrame( animate );
-    cube.rotation.x += 0.05;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;    
+    myfaceonabox.rotation.z += 0.01;
+    myfaceonabox.rotation.x += 0.01;
+
 	renderer.render( scene, camera );
 }
 animate();
+
+function addstar(){
+    const Stargeom = new THREE.SphereGeometry(0.25,24,24);
+    const StarMeterial = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(Stargeom, StarMeterial);
+
+    const [x, y , z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y , z);
+    scene.add(star)
+}
+Array(200).fill().forEach(addstar)
