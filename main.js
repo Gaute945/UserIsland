0f12e2e8b0b1fbd0b92a431a8d28ff2c5c2f04aa
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-10-31 12:52:04 +0100

    babooga
    prøvd å fått box på cyllinder

--- main.js
+++ main.js
@@ -20,18 +20,27 @@ document.body.appendChild(renderer.domElement);
 
 const textureLoad = new THREE.TextureLoader().load("mapofplace.jpg");
 const boksmap = new THREE.Mesh(
-  new THREE.BoxGeometry(23, 10, 0),
-  new THREE.MeshBasicMaterial({ wireframe: true })
+  new THREE.BoxGeometry(7, 6.8, 0),
+  new THREE.MeshBasicMaterial({ color: 0xff2d00,wireframe: true })
 );
 scene.add(boksmap);
-boksmap.position.set(0, 0, 0);
+boksmap.position.set(0, -2, 0);
 
 //geometry og material er det cube bruker til å bli cube utsende
-const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const material = new THREE.MeshBasicMaterial({ color: 0xff2d00 });
-//geometry meterial for cyllinder
-const geomeCy = new THREE.CylinderGeometry(0.3, 0.3, 0.3);
-const mateCy = new THREE.MeshBasicMaterial({ color: 0x2d9c18 });
+//geometry og material er det Svensker bruker til å bli Svensker utsende
+const SvenskG = new THREE.BoxGeometry(0.3,0.3,0.3);
+const SvenskF = new THREE.MeshBasicMaterial({ color: 0xff2d00 });
+//geometry meterial for normen
+const NorskG = new THREE.BoxGeometry(0.3, 0.3, 0.3);
+const NorskF = new THREE.MeshBasicMaterial({ color: 0x2d9c18 });
+//DanskF er meterial og DasnkG er Geimetry
+const DanskG = new THREE.BoxGeometry(0.3, 0.3, 0.3);
+const DanskF = new THREE.MeshBasicMaterial({ color: 0xe00ff });
+//FinskF og FinskG representerer geobox og metbasic til finland
+const FinskG = new THREE.BoxGeometry(0.3, 0.3, 0.3);
+const FinskF = new THREE.MeshBasicMaterial({ color: 0xa832a6 });
+
+let testnumbef = 1
 //geometry meterial for bakCyllinder
 const BakgeomeCy = new THREE.CylinderGeometry(1, 1, 1, 1, 1);
 const BakMatCy = new THREE.MeshBasicMaterial({ color: 0xcc330 })
@@ -40,22 +49,37 @@ const BakMatCy = new THREE.MeshBasicMaterial({ color: 0xcc330 })
 const befolkningSE = THREE.MathUtils.randFloat(1, 8);
 //for så lange i er mindre en befolkningse lager den fleire Svenskers
 for (let i = 0; i < befolkningSE; i++) {
-  let xPos = THREE.MathUtils.randFloat(7.5, -7.5);
-  let yPos = THREE.MathUtils.randFloat(3.2, -3.2);
+  let xPos = THREE.MathUtils.randFloat(1.875, -1.875);
+  let yPos = THREE.MathUtils.randFloat(1.1,-6);
   const Svensker = new THREE.Mesh(SvenskG, SvenskF);
   Svensker.position.set(xPos, yPos, 0);
   scene.add(Svensker);
 }
-
 //befolkning er kor mangen normen representert i cyllinder eg vil rendere i siden
 const befolkningNO = THREE.MathUtils.randFloat(1, 5);
 //for så lange j er mindre en befolkning se lager den fleire Svenskers
 for (let j = 0; j < befolkningNO; j++) {
   let xPos2 = THREE.MathUtils.randFloat(7.5, -7.5);
-  let yPos2 = THREE.MathUtils.randFloat(3.2, -3.2);
+  let yPos2 = THREE.MathUtils.randFloat(1.1,-6);
   const normen = new THREE.Mesh(NorskG, NorskF);
   normen.position.set(xPos2, yPos2, 0);
   scene.add(normen);
+}
+const befolkningDK = THREE.MathUtils.randFloat(1, 12);
+for (let j = 0; j < befolkningDK; j++) {
+  let xPos3 = THREE.MathUtils.randFloat(7.5, -7.5);
+  let yPos3 = THREE.MathUtils.randFloat(1.1,-6);
+  const Danske = new THREE.Mesh(DanskG, DanskF);
+  Danske.position.set(xPos3, yPos3, 0);
+  scene.add(Danske);
+}
+const befolkningFI = THREE.MathUtils.randFloat(1, 12);
+for (let k = 0; k < befolkningFI; k++) {
+  let xPos3 = THREE.MathUtils.randFloat(7.5, -7.5);
+  let yPos3 = THREE.MathUtils.randFloat(1.1,-6);
+  const finske = new THREE.Mesh(FinskG, FinskF);
+  finske.position.set(xPos3, yPos3, 0);
+  scene.add(finske);
 }
 
 const cylGeometry = new THREE.CylinderGeometry( 4, 5, 50, 128 ); 
@@ -68,21 +92,6 @@ scene.add( cylinder );
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
 
-/* for (let j = 0; j < befolkningDK; j++) {
-  let xPos3 = THREE.MathUtils.randFloat(7.5, -7.5);
-  let yPos3 = THREE.MathUtils.randFloat(3.2, -3.2);
-  const  = new THREE.Mesh(geomeCy, mateCy);
-  Cyllinder.position.set(xPos2, yPos2, 0);
-  scene.add(Cyllinder);
-} */
-
-/* camera.position.z = 10;
-camera.position.y = 80;
-camera.position.x = 0;
-camera.rotation.x = -1.55;
-camera.rotation.y = 0;
-camera.rotation.z = 0; */
-
 camera.position.z = 15;
 camera.position.y = -18;
 camera.position.x = 3.5;
@@ -91,7 +100,6 @@ camera.rotation.x = 1;
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  cube.rotateY(0.01);
-  cube.rotateX(0.01);
+  
 }
 animate();
