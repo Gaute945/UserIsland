84abf8eb0ad831bd9c7c47a2a2a987a9aab2f3b0
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-10-31 13:53:18 +0100

    fixed placement of boxes

--- main.js
+++ main.js
@@ -20,11 +20,11 @@ document.body.appendChild(renderer.domElement);
 
 const textureLoad = new THREE.TextureLoader().load("mapofplace.jpg");
 const boksmap = new THREE.Mesh(
-  new THREE.BoxGeometry(7, 6.8, 0),
+  new THREE.BoxGeometry(10, 8.8, 0),
   new THREE.MeshBasicMaterial({ color: 0xff2d00,wireframe: true })
 );
 scene.add(boksmap);
-boksmap.position.set(0, -2, 0);
+boksmap.position.set(0, -3, 0);
 
 //geometry og material er det cube bruker til å bli cube utsende
 //geometry og material er det Svensker bruker til å bli Svensker utsende
@@ -41,52 +41,49 @@ const FinskG = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 const FinskF = new THREE.MeshBasicMaterial({ color: 0xa832a6 });
 
 let testnumbef = 1
-//geometry meterial for bakCyllinder
-const BakgeomeCy = new THREE.CylinderGeometry(1, 1, 1, 1, 1);
-const BakMatCy = new THREE.MeshBasicMaterial({ color: 0xcc330 })
 
 //befolkningse er kor mangen cube/svensker eg vil rendere i siden
-const befolkningSE = THREE.MathUtils.randFloat(1, 8);
+const befolkningSE = THREE.MathUtils.randFloat(1, 80);
 //for så lange i er mindre en befolkningse lager den fleire Svenskers
 for (let i = 0; i < befolkningSE; i++) {
-  let xPos = THREE.MathUtils.randFloat(1.875, -1.875);
-  let yPos = THREE.MathUtils.randFloat(-6,4);
+  let xPos = THREE.MathUtils.randFloat(-4,4.5);
+  let yPos = THREE.MathUtils.randFloat(-7,2);
   const Svensker = new THREE.Mesh(SvenskG, SvenskF);
   Svensker.position.set(xPos, yPos, 0);
   scene.add(Svensker);
 }
 
 //befolkning er kor mangen normen representert i cyllinder eg vil rendere i siden
-const befolkningNO = THREE.MathUtils.randFloat(1, 5);
+const befolkningNO = THREE.MathUtils.randFloat(1, 50);
 //for så lange j er mindre en befolkning se lager den fleire Svenskers
 for (let j = 0; j < befolkningNO; j++) {
-  let xPos2 = THREE.MathUtils.randFloat(3,-3);
-  let yPos2 = THREE.MathUtils.randFloat(-6,4);
+  let xPos2 = THREE.MathUtils.randFloat(-4,4.5);
+  let yPos2 = THREE.MathUtils.randFloat(-7,2);
   const normen = new THREE.Mesh(NorskG, NorskF);
   normen.position.set(xPos2, yPos2, 0);
   scene.add(normen);
 }
 
-const befolkningDK = THREE.MathUtils.randFloat(1, 12);
+const befolkningDK = THREE.MathUtils.randFloat(1, 120);
 for (let j = 0; j < befolkningDK; j++) {
-  let xPos3 = THREE.MathUtils.randFloat(3,-3);
-  let yPos3 = THREE.MathUtils.randFloat(-6,4);
+  let xPos3 = THREE.MathUtils.randFloat(-4,4.5);
+  let yPos3 = THREE.MathUtils.randFloat(-7,2);
   const Danske = new THREE.Mesh(DanskG, DanskF);
   Danske.position.set(xPos3, yPos3, 0);
   scene.add(Danske);
 }
 
-const befolkningFI = THREE.MathUtils.randFloat(1, 12);
+const befolkningFI = THREE.MathUtils.randFloat(1, 30);
 for (let k = 0; k < befolkningFI; k++) {
-  let xPos3 = THREE.MathUtils.randFloat(3,-3);
-  let yPos3 = THREE.MathUtils.randFloat(-6,4);
+  let xPos3 = THREE.MathUtils.randFloat(-4,4.5);
+  let yPos3 = THREE.MathUtils.randFloat(-7,2);
   const finske = new THREE.Mesh(FinskG, FinskF);
   finske.position.set(xPos3, yPos3, 0);
   scene.add(finske);
 }
 
-const cylGeometry = new THREE.CylinderGeometry( 6, 7, 50, 128 ); 
-const cylMaterial = new THREE.MeshBasicMaterial({ map: textureLoad }); 
+const cylGeometry = new THREE.CylinderGeometry( 6, 7, 49, 128 ); 
+const cylMaterial = new THREE.MeshBasicMaterial({  map: textureLoad, }); 
 const cylinder = new THREE.Mesh( cylGeometry, cylMaterial ); 
 cylinder.rotation.x = -1.5
 cylinder.position.z = -25.2
