811c33629c1fffbcfab6115d542180dea6b01c7d
SigHelLun <116567255+SigHelLun@users.noreply.github.com>
2023-11-07 13:22:11 +0100

    update all four group animate

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
@@ -7,7 +7,7 @@ const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000,
+  1000
 );
 const helper = new THREE.CameraHelper(camera);
 scene.add(helper);
@@ -17,7 +17,9 @@ const renderer = new THREE.WebGLRenderer({ antialiasing: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const textureLoad = new THREE.TextureLoader().load("Resource/1200px-Scandinavia_regions_map-567632390.png");
+const textureLoad = new THREE.TextureLoader().load(
+  "Resource/1200px-Scandinavia_regions_map-567632390.png"
+);
 /* const textureLoad = new THREE.TextureLoader().load("Resource/tex_DebugGrid.png"); */
 const boksmap = new THREE.Mesh(
   new THREE.BoxGeometry(10, 8.8, 0),
@@ -30,7 +32,7 @@ const seg = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 const sef = new THREE.MeshBasicMaterial({ color: 0xff2d00 });
 
 const nog = new THREE.BoxGeometry(0.3, 0.3, 0.3);
-const nof = new THREE.MeshBasicMaterial({ color: 0x2d9c18 });
+const nof = new THREE.MeshBasicMaterial({ color: 0x2d9c18 });//green
 
 const dkg = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 const dkf = new THREE.MeshBasicMaterial({ color: 0xe00ff });
@@ -38,12 +40,10 @@ const dkf = new THREE.MeshBasicMaterial({ color: 0xe00ff });
 const fig = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 const fif = new THREE.MeshBasicMaterial({ color: 0xa832a6 });
 
+let se;
+let animateSe = [];
 
-
-let se
-let animateSe = []
-
-const populationSe = 5//THREE.MathUtils.randFloat(1, 8);
+const populationSe = THREE.MathUtils.randFloat(1, 8);
 
 for (let i = 0; i < populationSe; i++) {
   let xPos = THREE.MathUtils.randFloat(-4, 4.5);
@@ -51,13 +51,13 @@ for (let i = 0; i < populationSe; i++) {
   se = new THREE.Mesh(seg, sef);
   se.position.set(xPos, yPos, 0);
   scene.add(se);
-  animateSe.push(se)
+  animateSe.push(se);
 }
 
-let no
-let animateNo = []
+let no;
+let animateNo = [];
 
-const populationNo = 0//THREE.MathUtils.randFloat(1, 5);
+const populationNo = THREE.MathUtils.randFloat(1, 5);
 
 for (let j = 0; j < populationNo; j++) {
   let xPos2 = THREE.MathUtils.randFloat(-4, 4.5);
@@ -65,11 +65,11 @@ for (let j = 0; j < populationNo; j++) {
   no = new THREE.Mesh(nog, nof);
   no.position.set(xPos2, yPos2, 0);
   scene.add(no);
-  animateNo.push(no)
+  animateNo.push(no);
 }
 
-let dk
-let animateDk = []
+let dk;
+let animateDk = [];
 
 const populationDk = THREE.MathUtils.randFloat(1, 12);
 for (let j = 0; j < populationDk; j++) {
@@ -78,19 +78,19 @@ for (let j = 0; j < populationDk; j++) {
   dk = new THREE.Mesh(dkg, dkf);
   dk.position.set(xPos3, yPos3, 0);
   scene.add(dk);
-  animateDk.push(dk)
+  animateDk.push(dk);
 }
 
-let fi
-let animateFi = []
-const populationFi = 0//THREE.MathUtils.randFloat(1, 3);
+let fi;
+let animateFi = [];
+const populationFi = THREE.MathUtils.randFloat(1, 3);
 for (let k = 0; k < populationFi; k++) {
   let xPos3 = THREE.MathUtils.randFloat(-4, 4.5);
   let yPos3 = THREE.MathUtils.randFloat(-7, 2);
   fi = new THREE.Mesh(fig, fif);
   fi.position.set(xPos3, yPos3, 0);
   scene.add(fi);
-  animateFi.push(fi)
+  animateFi.push(fi);
 }
 
 document.getElementById("no").innerHTML = "No " + Math.ceil(populationNo);
@@ -99,11 +99,11 @@ document.getElementById("dk").innerHTML = "Dk " + Math.ceil(populationDk);
 document.getElementById("fi").innerHTML = "Fi " + Math.ceil(populationFi);
 
 const cylG = new THREE.CylinderGeometry(6, 7, 49, 128);
-const cylM = new THREE.MeshBasicMaterial({ map: textureLoad, });
+const cylM = new THREE.MeshBasicMaterial({ map: textureLoad });
 const cylinder = new THREE.Mesh(cylG, cylM);
-cylinder.rotation.x = -1.5
-cylinder.rotation.y = 1.5
-cylinder.position.z = -25.5
+cylinder.rotation.x = -1.5;
+cylinder.rotation.y = 1.5;
+cylinder.position.z = -25.5;
 scene.add(cylinder);
 
 const axesHelper = new THREE.AxesHelper(50);
@@ -114,49 +114,85 @@ camera.position.y = -18;
 camera.position.x = 0;
 camera.rotation.x = 1;
 
-//delta time
-let clock = new THREE.Clock();
-let speed = 2;
-let delta = 0;
-let speeds = [];
-
 // for stop condition
 const Lx = -5;
 const Rx = 5;
 const maxy = 1.5;
 const miny = -7.2;
 
+//delta time
+let clock = new THREE.Clock();
+let speed = 2;
+let delta = 0;
+let speedsSe = [];
+let speedsNo = [];
+let speedsDk = [];
+let speedsFi = [];
 //how fast will object go indevidually
 for (let i = 0; i < animateSe.length; i++) {
-    speeds[i] = 1;
+  speedsSe[i] = 1;
 }
-
+for (let i = 0; i < animateNo.length; i++) {
+  speedsNo[i] = 1;
+}
+for (let i = 0; i < animateDk.length; i++) {
+  speedsDk[i] = 1;
+}
+for (let i = 0; i < animateFi.length; i++) {
+  speedsFi[i] = 1;
+}
 function animate() {
   requestAnimationFrame(animate);
 
   delta = clock.getDelta();
-  //
+  //sweden animastion
   for (let i = 0; i < animateSe.length; i++) {
-    animateSe[i].position.x += speeds[i] * delta;
+    animateSe[i].position.x += speedsSe[i] * delta;
 
     if (animateSe[i].position.x > Rx || animateSe[i].position.x < Lx) {
-        animateSe[i].position.x = Math.min(Rx, Math.max(Lx, animateSe[i].position.x));
-        speeds[i] *= -1;
+      animateSe[i].position.x = Math.min(
+        Rx,
+        Math.max(Lx, animateSe[i].position.x)
+      );
+      speedsSe[i] *= -1;
+    }
+  }
+  //Norway animastion
+  for (let i = 0; i < animateNo.length; i++) {
+    animateNo[i].position.x += speedsNo[i] * delta;
+
+    if (animateNo[i].position.x > Rx || animateNo[i].position.x < Lx) {
+      animateNo[i].position.x = Math.min(
+        Rx,
+        Math.max(Lx, animateNo[i].position.x)
+      );
+      speedsNo[i] *= -1;
     }
   }
 
-  for (let i = 0; i < animateNo.length; i++) {
+  for (let i = 0; i < animateDk.length; i++) {
+    animateDk[i].position.y += speedsDk[i] * delta;
+
+    if (animateDk[i].position.y > maxy || animateDk[i].position.y < miny) {
+      animateDk[i].position.y = Math.min(
+        maxy,
+        Math.max(miny, animateDk[i].position.y)
+      );
+      speedsDk[i] *= -1;
+    }
   }
 
-/*   for (let i = 0; i < animateDk.length; i++) {
-    animateDk[i].rotation.x += 1 * delta;
-    animateDk[i].rotation.y += 1 * delta;
+  for (let i = 0; i < animateFi.length; i++) {
+    animateFi[i].position.y += speedsFi[i] * delta;
+
+    if (animateFi[i].position.y > maxy || animateFi[i].position.y < miny) {
+      animateFi[i].position.y = Math.min(
+        maxy,
+        Math.max(miny, animateFi[i].position.y)
+      );
+      speedsFi[i] *= -1;
+    }
   }
-  
-    for (let i = 0; i < animateFi.length; i++) {
-    animateFi[i].rotation.x += 0.01 * delta;
-    animateFi[i].rotation.y += 0.01 * delta;
-  } */
   renderer.render(scene, camera);
 }
 
